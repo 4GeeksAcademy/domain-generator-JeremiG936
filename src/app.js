015aa26cf460bbe1124066f189cce5ba4bc5e046
JeremiG936 <jeremigonzalez1@gmail.com>
2025-04-16 04:15:29 +0000

    Se realizo domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+function randomNumber (min, max) {
+  return Math.floor(Math.random() * (max -min)) + min;
+}
+
+function getRandom (arr) {
+  let min = 0;
+  let max = arr.length;
+  let index = randomNumber(0, arr.length);
+  return arr[index];
+}
+
+function myLoop () {
+  let nestedFunction = getRandom;
+  for (let i = 0; i < 8; i++) {
+    let domain = getRandom(pronoun) + getRandom(adj) + getRandom(noun) +".com";
+    return domain;
+  }
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector('#domain').innerHTML = myLoop();
 };
