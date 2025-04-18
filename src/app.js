b3ef5747460fca82b7773cdd2eb7ae5a6f23db72
JeremiG936 <jeremigonzalez1@gmail.com>
2025-04-18 22:19:22 +0000

    Se realizon ajustes

--- src/app.js
+++ src/app.js
@@ -2,23 +2,20 @@ let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 
-function randomNumber (min, max) {
-  return Math.floor(Math.random() * (max -min)) + min;
-}
-
-function getRandom (arr) {
-  let min = 0;
-  let max = arr.length;
-  let index = randomNumber(0, arr.length);
-  return arr[index];
-}
-
 function myLoop () {
-  let nestedFunction = getRandom;
-  for (let i = 0; i < 8; i++) {
-    let domain = getRandom(pronoun) + getRandom(adj) + getRandom(noun) +".com";
-    return domain;
+  let domains = [];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        let domain = `${p}${a}${n}.com`;
+        console.log(domain);
+        domains.push(domain);
+      }
+    }
   }
+
+  return domains;
 }
 
 
